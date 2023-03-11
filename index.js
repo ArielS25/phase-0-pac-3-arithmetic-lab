b80346b936c64b5e255781421065dff3f8882cfa
Jorge Ariel Sanchez Leon <qwertyice2552@gmail.com>
2023-03-10 22:32:22 -0700

    lab complete

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    return a + b;
+}
+add(1560, 1)
+function subtract(a, b){
+    return a - b;
+}
+subtract(660, 1)
+function multiply(a, b){
+    return a * b;
+}
+multiply(78532, 4)
+function divide(a, b){
+    return a / b;
+}
+divide(16.2307692308, 4)
+function increment(n){
+    return (n + 1);
+}
+increment();
+function decrement(n){
+    return n - 1;
+}
+decrement();
+function makeInt(n) {
+    return parseInt(n, 10)
+ }
+ function preserveDecimal(n){
+    return parseFloat(n);
+ }
